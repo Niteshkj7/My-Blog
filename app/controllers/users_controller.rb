9002c91d06f4f3a8d5bfaa51220dc0234b416408
Nitesh <nitesh@hitesh.co.in>
2018-03-13 15:24:57 +0530

    partial signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  def index
+    @user = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+
+
+  private 
+
+    def user_params
+       params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
